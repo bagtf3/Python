ba36dfa8a54666eaf4ad6183231324aebd06eb79
Bryan Goggin <24681738+bagtf3@users.noreply.github.com>
2017-08-06 02:44:00 -0500

    Update reverseParenthesis.py

--- CodeSamples/CodeFights/reverseParenthesis.py
+++ CodeSamples/CodeFights/reverseParenthesis.py
@@ -1,3 +1,11 @@
+#You have a string s that consists of English letters, punctuation marks, whitespace characters, and brackets.
+#It is guaranteed that the parentheses in s form a regular bracket sequence.
+#Your task is to reverse the strings contained in each pair of matching parentheses, starting from the innermost pair. 
+#The results string should not contain any parentheses.
+#Example:
+#For string s = "a(bc)de", the output should be
+#reverseParentheses(s) = "acbde".
+
 def reverseParentheses(s):
     npars = len([c for c in s if c == "("])
     newstr = ""
@@ -7,10 +15,8 @@ def reverseParentheses(s):
     
     if npars == 0:
         return s
-    
     else:    
         for c in s:
-                
             if pcount == npars and stop == 0:
                 if not c == ")":
                     addstr = c + addstr
